f0a4e9585ed4695086975d476d9f03f47d43f1b3
Callsign-Neptune <66689267+Callsign-Neptune@users.noreply.github.com>
2020-08-18 08:44:49 -0500

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_Sakila.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
